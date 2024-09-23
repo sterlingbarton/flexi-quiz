7b034adf39f404b5207afb6f56d7ee6529e05ef9
Sterling <sterlingbarton81@gmail.com>
2024-09-23 15:26:21 -0500

    added onclick functionality for log in and sign up

--- flexi-path/src/components/buttons/log-in-btn/LogInBtn.tsx
+++ flexi-path/src/components/buttons/log-in-btn/LogInBtn.tsx
@@ -1,10 +1,17 @@
 import { Button } from '@mui/material';
 import LogInBtnStyles from './LogInBtnStyles';
+import { useNavigate } from 'react-router-dom';
 
 export function LogInBtn() {
   const styles = LogInBtnStyles();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/log-in');
+  };
+
   return (
-    <Button variant="outlined" sx={styles.logInBtn}>
+    <Button variant="outlined" sx={styles.logInBtn} onClick={handleClick}>
       Log In
     </Button>
   );
@@ -12,8 +19,14 @@ export function LogInBtn() {
 
 export function DarkNavLogInBtn() {
   const styles = LogInBtnStyles();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/log-in');
+  };
+
   return (
-    <Button variant="text" sx={styles.darkLogInBtn}>
+    <Button variant="text" sx={styles.darkLogInBtn} onClick={handleClick}>
       Log In
     </Button>
   );
--- flexi-path/src/components/buttons/sign-up-btn/SignUpBtn.tsx
+++ flexi-path/src/components/buttons/sign-up-btn/SignUpBtn.tsx
@@ -1,10 +1,17 @@
 import { Button } from '@mui/material';
 import SignUpBtnStyles from './SignUpBtnStyles';
+import { useNavigate } from 'react-router-dom';
 
 export function SignUpBtn() {
   const styles = SignUpBtnStyles();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/sign-up');
+  };
+
   return (
-    <Button variant="contained" sx={styles.signUpBtn}>
+    <Button variant="contained" sx={styles.signUpBtn} onClick={handleClick}>
       Sign up
     </Button>
   );
@@ -12,8 +19,18 @@ export function SignUpBtn() {
 
 export function DarkNavSignUpBtn() {
   const styles = SignUpBtnStyles();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/sign-up');
+  };
+
   return (
-    <Button variant="outlined" sx={styles.darkNavSignUpBtn}>
+    <Button
+      variant="outlined"
+      sx={styles.darkNavSignUpBtn}
+      onClick={handleClick}
+    >
       Sign up
     </Button>
   );
